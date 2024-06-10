348293cadc855b8b118b855654bd6765803cb0d8
Dan <99934016+dancerb@users.noreply.github.com>
2024-06-10 14:50:56 -0700

    Update cv.pdf and Hero component

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,5 +1,5 @@
 import styles from './HeroStyles.module.css';
-import heroImg from '../../assets/Design uten navn.png';
+import heroImg from '../../assets/memoji.png';
 import sun from '../../assets/sun.svg';
 import moon from '../../assets/moon.svg';
 import twitterLight from '../../assets/twitter-light.svg';
@@ -25,7 +25,7 @@ function Hero() {
         <img
           src={heroImg}
           className={styles.hero}
-          alt="Profile picture of Harris Johnsen"
+          alt="Profile picture of Daniel Nguyen"
         />
         <img
           className={styles.colorMode}
@@ -36,9 +36,9 @@ function Hero() {
       </div>
       <div className={styles.info}>
         <h1>
-          Harris
+          Daniel
           <br />
-          Johnsen
+          Nguyen
         </h1>
         <h2>Frontend Developer</h2>
         <span>
